5fe78b4b39de8de5d46c2ec245049a216476f3d3
Inevitable200tech <abhishek2007tech@gmail.com>
2025-05-29 16:35:36 +0530

    Initial commit

--- app.js
+++ app.js
@@ -1821,7 +1821,7 @@ app.get('/api/game-script/:id', async (req, res) => {
 });
 
 app.get('/game-detail/:id', (req, res) => {
-  res.sendFile('public/details.html', { root: __dirname });
+  res.send('details.html');
 });
 
 // Server Start
